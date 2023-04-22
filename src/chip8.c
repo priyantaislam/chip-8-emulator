017b8c55d9941f5473370992d0a7ebbd952b313a
Priyanta Islam <islam.priyanta@gmail.com>
2023-04-22 23:03:02 +1000

    implementing opcodes

--- src/chip8.c
+++ src/chip8.c
@@ -142,6 +142,21 @@ static void chip8_exec_extended(struct chip8* chip8, unsigned short opcode) {
         case 0x8000:
             chip8_exec_extended_eight(chip8, opcode);
             break;
+        //9xy0 SNE
+        case 0x9000:
+        if(chip8->registers.V[x] != chip8->registers.V[y]){
+            chip8->registers.PC += 2;
+        }
+        break;
+        //Annn
+        case 0xA000:
+        chip8->registers.I = nnn;
+        break;
+        //bnnn 0 Jump to location nnn + V0
+        case 0xB000:
+            chip8->registers.PC = nnn + chip8->registers.V[0x00];
+        break;
+        
     }
 }
 
